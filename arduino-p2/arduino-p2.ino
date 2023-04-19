75ea4797b1f7cad1214e208ed58414f80a9731f8
Martin Calderón <martinrolo22@estudiantec.cr>
2023-04-18 21:53:41 -0600

    Update arduino-p2.ino

--- arduino-p2/arduino-p2.ino
+++ arduino-p2/arduino-p2.ino
@@ -1,14 +1,29 @@
-int randomNumber;
-
+const int trig = 2;   
+const int echo = 3;   
+long tiempo; 
+long distancia;
+const float sonido = 34300.0; // Velocidad del sonido en cm/s
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);//iniciailzamos la comunicación
+  pinMode(trig, OUTPUT); //pin como salida
+  pinMode(echo, INPUT);  //pin como entrada
+  digitalWrite(trig, LOW);//Inicializamos el pin con 0
 }
 
-void loop() {
+void loop()
+{
 
-  randomNumber=random(100);
-  Serial.print(randomNumber);
- 
-  delay(1500);
+  
+
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);          
+  digitalWrite(trig, LOW);
+  
+  tiempo = pulseIn(echo, HIGH); 
+  distancia = tiempo * 0.000001 * sonido/2;             
+
+  Serial.print(distancia);      
+
+  delay(1000);          
 }
