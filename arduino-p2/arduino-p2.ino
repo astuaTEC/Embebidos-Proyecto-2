04eacc50fd9cd17e2fec38b216c847f4a947eace
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2023-04-20 15:23:27 -0600

    Cambio en sensor arduino

--- arduino-p2/arduino-p2.ino
+++ arduino-p2/arduino-p2.ino
@@ -1,29 +1,12 @@
-const int trig = 2;   
-const int echo = 3;   
-long tiempo; 
-long distancia;
-const float sonido = 34300.0; // Velocidad del sonido en cm/s
+int porcentaje;
 
 void setup() {
-  Serial.begin(115200);//iniciailzamos la comunicación
-  pinMode(trig, OUTPUT); //pin como salida
-  pinMode(echo, INPUT);  //pin como entrada
-  digitalWrite(trig, LOW);//Inicializamos el pin con 0
+  Serial.begin(115200);
 }
 
-void loop()
-{
-
+void loop() {
   
-
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);          
-  digitalWrite(trig, LOW);
-  
-  tiempo = pulseIn(echo, HIGH); 
-  distancia = tiempo * 0.000001 * sonido/2;             
-
-  Serial.print(distancia);      
-
-  delay(700);          
+  porcentaje = map(analogRead(0), 0, 1023, 0, 100);
+  Serial.print(porcentaje);
+  delay(700);
 }
